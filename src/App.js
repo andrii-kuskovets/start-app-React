ab885dd61502a37a16b5435e34cebbfc95570811
Andrii <karuks199459@gmail.com>
2020-05-11 13:46:23 +0300

    Add component promo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from "./components/header/Header";
+import Promo from "./components/promo/Promo";
 import './assets/scss/style.scss'
 
 function App() {
   return (
       <div>
         <Header />
+        <Promo />
       </div>
   );
 }
---
+++ src/components/promo/Promo.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './_promo.scss';
+
+function Promo() {
+    return (
+        <section className="promo section">
+            <div className="container br-lg">
+                <div className="promo__content">
+                    <div className="promo__description">
+                        <div className="section__heading">
+                            <h1 className="section__title section__title_hero promo__title">
+                                Product Design Course
+                                <span className="section__subtitle">Product Factory</span>
+                                <span className="section__back-title">UI / UX</span>
+                            </h1>
+                            <p className="section__paragraph section__paragraph_big">
+                                Learn how design thinking, user research,
+                                business vision and marketing, and finally designing
+                                and creating real digital products for real users.
+                            </p>
+                        </div>
+                        <button className="btn btn__main">Start Register</button>
+                    </div>
+                    <div className="promo__img">
+                        <h1 className="section__title section__title_hero promo__title promo__title_mobile">
+                            Product Design Course
+                            <span className="section__subtitle">Product Factory</span>
+                            <span className="section__back-title">UI / UX</span>
+                        </h1>
+                        <img src={require("../../assets/img/promo.png")} alt='factory'></img>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default Promo;
