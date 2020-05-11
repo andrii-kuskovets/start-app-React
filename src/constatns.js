2bc1c58318fe975d0eae6b5ae23089ee12fabaec
Andrii <karuks199459@gmail.com>
2020-05-11 11:35:51 +0300

    Add component header

---
+++ src/constatns.js
@@ -0,0 +1,30 @@
+export const NAV_ITEMS = [
+  {
+    title: 'Projects',
+    link: 'projects'
+  },
+  {
+    title: 'Partners',
+    link: 'partners'
+  },
+  {
+    title: 'FAQ',
+    link: 'faq'
+  },
+  {
+    title: 'Masters',
+    link: 'masters'
+  },
+  {
+    title: 'Classes',
+    link: 'classes'
+  },
+  {
+    title: 'Course Experience',
+    link: 'experience'
+  },
+  {
+    title: 'About us',
+    link: 'about'
+  },
+]
